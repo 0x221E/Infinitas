dde2d5b690a265d284b00d407878ee7c6eecb0b6
0x221E <ozdemir.demir@protonmail.com>
2025-11-06 17:22:40 +0100

    Refactor: Changed logging system from spdlog to a native solution, error messages display file name and line numbers.

--- infinitas/src/init.cpp
+++ infinitas/src/init.cpp
@@ -5,6 +5,5 @@ namespace GlobalEnvironment
     void Initialize()
     {
        mp::mpf_float::default_precision(1000);
-       spdlog::set_level(spdlog::level::trace);
     }
 }
--- shared/include/shared/logger.h
+++ shared/include/shared/logger.h
@@ -1,14 +1,82 @@
 #pragma once
 
-#include <spdlog/spdlog.h>
+#include <source_location>
+#include <print>
+
+namespace shared
+{
+    enum class LogLevel
+    {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        CRITICAL
+    };
+
+    template<LogLevel level, class...ARGS>
+    struct Log{};
+
+    template<LogLevel level, class...ARGS>
+    struct Log<level, const char*, ARGS...>
+    {
+        Log(const char* message, ARGS...args, std::source_location loc = std::source_location::current())
+        {
+            std::println("[{}] [{}-l{}]: {}", GetLevel(), loc.file_name(), loc.line(), std::vformat(message, std::make_format_args(args...)));
+        }
+
+        std::string_view GetLevel()
+        {
+            if constexpr(level == LogLevel::DEBUG)
+            {  
+                return "Debug";
+            }else if constexpr(level == LogLevel::INFO)
+            {
+                return "Info"; 
+            }
+            else if constexpr(level == LogLevel::WARN)
+            {
+                return "Warning";
+            }
+            else if constexpr(level == LogLevel::ERROR)
+            {
+                return "Error";
+            }
+            else if constexpr(level == LogLevel::CRITICAL)
+            {
+                return "Critical";
+            }
+            return "Unknown";
+        }
+    };
+
+
+    template<LogLevel L, class...ARGS>
+    Log(ARGS...) -> Log<L, ARGS...>;
+
+    template<class...ARGS>
+    using DebugLog = Log<LogLevel::DEBUG, ARGS...>;
+
+    template<class...ARGS>
+    using InfoLog = Log<LogLevel::INFO, ARGS...>;
+
+    template<class...ARGS>
+    using WarnLog = Log<LogLevel::WARN, ARGS...>;
+
+    template<class...ARGS>
+    using ErrorLog = Log<LogLevel::ERROR, ARGS...>;
+
+    template<class...ARGS>
+    using CriticalLog = Log<LogLevel::CRITICAL, ARGS...>;
+
+}
 
 #ifdef Debug
-    #define LOG_TRACE(...) :: spdlog::trace(__VA_ARGS__)
-    #define LOG_DEBUG(...) :: spdlog::debug(__VA_ARGS__)
-    #define LOG_INFO(...) :: spdlog::info(__VA_ARGS__)
-    #define LOG_WARN(...) :: spdlog::warn(__VA_ARGS__)
-    #define LOG_ERROR(...) :: spdlog::error(__VA_ARGS__)
-    #define LOG_CRITICAL(...) :: spdlog::critical(__VA_ARGS__)
+    #define LOG_DEBUG(...) :: shared::DebugLog(__VA_ARGS__)
+    #define LOG_INFO(...) :: shared::InfoLog(__VA_ARGS__)
+    #define LOG_WARN(...) :: shared::WarnLog(__VA_ARGS__)
+    #define LOG_ERROR(...) :: shared::ErrorLog(__VA_ARGS__)
+    #define LOG_CRITICAL(...) :: shared::CriticalLog(__VA_ARGS__)
 #else
     #define LOG_TRACE(...)
     #define LOG_DEBUG(...) 
@@ -18,10 +86,4 @@
     #define LOG_CRITICAL(...)  
 #endif
 
-#define LOG_PRODUCTION(...) :: spdlog::info(__VA_ARGS__)
-
-#if defined(CM_DEBUG)
-    #define LOG_CM(...) :: spdlog::debug(__VA_ARGS__)
-#else
-    #define LOG_CM(...)
-#endif
+#define LOG_PRODUCTION(...) :: shared::InfoLog(__VA_ARGS__)
